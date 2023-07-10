fcdf04f9b74eab07f0858453a54c69b437d2f808
DanVerh <danverh29@gmail.com>
2023-07-10 18:06:46 +0300

    start scripts finished && volume to DB added

--- start.ps1
+++ start.ps1
@@ -1,24 +1,26 @@
 # Create a separate network for the application
-#docker network create --subnet 172.18.0.1/29 --driver bridge app-network
+docker network create --subnet 172.18.0.1/29 --driver bridge app-network
+Write-Host "Network for the app is created"
 
 # Build and start the PostgreSQL DB
-#cd ./db
-#docker image build -t db-image --rm .
-#docker container run -d --name db --network app-network --ip 172.18.0.2 db-image
+cd ./db
+docker image build -t db-image --rm .
+docker container run -d --name db --network app-network --ip 172.18.0.2 db-image
+cd ..
+Write-Host "DB container is up an running"
 
 # Build and start the Go backend
-#docker container rm backend -f
-#cd ./backend
-#docker image build -t backend-image --rm .
-#docker container run -d -p 8080:8080 --network app-network --name backend backend-image
-#docker container logs backend 
-#cd ..
+cd ./backend
+docker image build -t backend-image --rm .
+docker container run -d -p 8080:8080 --network app-network --name backend backend-image
+docker container logs backend 
+cd ..
+Write-Host "Backend container is up an running"
 
 # Build and start the React on Nginx frontend
 cd ./frontend
-docker container rm frontend -f
 docker image build -t frontend-image --rm .
-docker container run -d -p 80:80 --name frontend frontend-image
+docker container run -d -p 80:80 --network app-network --name frontend frontend-image
 cd ..
 
-echo "Site is running on localhost:80"
+Write-Host "Frontend is running on localhost:80"
--- start.sh
+++ start.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-# Build and start the Go backend
+# Create a separate network for the application
+docker network create --subnet 172.18.0.1/29 --driver bridge app-network
+echo "Network for the app is created"
+
+# Build and start the PostgreSQL DB
 cd ./db
 docker image build -t db-image --rm .
-docker container run -d --name db db-image
+docker container run -d --name db --network app-network --ip 172.18.0.2 db-image
+cd ..
+echo "DB container is up an running"
 
 # Build and start the Go backend
-cd ../backend
+cd ./backend
 docker image build -t backend-image --rm .
-docker container run -d -p 8080:8080 --name backend backend-image
+docker container run -d -p 8080:8080 --network app-network --name backend backend-image
+docker container logs backend 
+cd ..
+echo "Backend container is up an running"
 
 # Build and start the React on Nginx frontend
-#cd ../frontend
-#docker image build -t frontend-image --rm .
-#docker container run -d -p 80:80 --name frontend frontend-image
-#
-#echo "Site is running on localhost:80"
+cd ./frontend
+docker image build -t frontend-image --rm .
+docker container run -d -p 80:80 --network app-network --name frontend frontend-image
+cd ..
+
+echo "Frontend is running on localhost:80"
